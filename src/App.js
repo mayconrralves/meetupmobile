c209cce48eb1bf45db97a24c45891a92e2c47dd1
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2020-12-26 12:46:42 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+import SignIn from './screens/SignIn';
+export default function App(){
+	return (
+			<View>
+				<SignIn />
+			</View>
+		)
+}
