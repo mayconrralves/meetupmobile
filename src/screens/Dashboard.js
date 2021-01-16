a5ab6975172f33e9f4d1a842f3d5db5936e1ae68
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-01-15 21:42:11 -0300

    Tela Dashboard com todas as funcionalidades

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,13 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Icon from 'react-native-vector-icons/AntDesign'
 import { connect } from 'react-redux';
-import { requestMeetups} from '../store/modules/meet/actions';
+import DatePicker from 'react-native-date-picker';
 import { useIsFocused } from '@react-navigation/native';
+import { format, add, sub, isPast, isToday, parseISO, isSameDay } from 'date-fns';
+import { zonedTimeToUtc  } from 'date-fns-tz';
+import  pt  from 'date-fns/locale/pt-BR';
+import { requestMeetups} from '../store/modules/meet/actions';
+
+
 import { 
 	ScrollView,
 	View,
 	Text,
 	TextInput,
 	TouchableOpacity,
+	TouchableHighlight,
+	Modal,
 	Image,
 	StyleSheet
 } from 'react-native';
@@ -17,7 +26,31 @@ import Header from '../components/Header';
 
 
 export function Dashboard( { meets, getMeets } ) {
+	
+	const [modalVisible, setModalVisible] = useState(false);
+	const [dateActual, setDateActual] = useState(new Date());
 	const isFocused = useIsFocused();
+	const addDate = () => {
+		setDateActual(add(dateActual, { days: 1}));
+	}
+	const subDate = () => {
+		setDateActual(sub(dateActual, { days: 1}))
+	}
+	const formatedDate = () => {
+		return format(dateActual, "dd/MM/yyyy")
+	}
+	const formatedDateText = (date) => {
+		return format(
+			zonedTimeToUtc(parseISO(date), 'America/Sao Paulo'),
+			 "dd, 'de' MMMM, '`as', HH'h'", 
+			 { locale: pt}
+		)
+	}
+	const dateChoose = (date) => {
+		if(!isPast(date) || isToday(date)) {
+			setDateActual(date);
+		}
+	}
 	useEffect(()=> {
 		if(isFocused) {
 			getMeets();
@@ -28,17 +61,67 @@ export function Dashboard( { meets, getMeets } ) {
 		<Background>
 			<View style={styles.dashboard}>
 				<Header />
-				<View style={styles.menu}><Text>31 de janeiro</Text></View>
+				<View style={styles.menu}>
+					<Modal
+				        animationType="slide"
+				        transparent={true}
+				        visible={modalVisible}
+				        style={styles.modal}
+				      >
+				      <View style={styles.modalCenter}>
+				      		 <View style={styles.modalView}>
+					      	<DatePicker
+						      			style={styles.datePicker}
+		      							date={dateActual}
+		      							onDateChange={dateChoose}
+		      							mode='date'
+		      							textColor='#fff'
+		    					/>
+		    					<TouchableOpacity style={styles.buttonModal}
+		    							onPress={() => setModalVisible(!modalVisible)}
+		    						>
+		    						<Text style={styles.textButtonModal}> Ok </Text>
+		    					</TouchableOpacity>
+					      </View>
+				      </View>
+				     
+				     </Modal>
+					<TouchableOpacity
+						onPress={()=> subDate()}
+					>
+						<Icon name='left' size={35} color='#fff'/>
+					</TouchableOpacity>
+					<TouchableHighlight
+						onPress={()=>setModalVisible(true)}
+					>
+						<Text style={styles.textMenu}>
+						{
+							formatedDate()
+						}
+					</Text>
+					</TouchableHighlight>
+					
+					
+					<TouchableOpacity
+						onPress={() => addDate()}
+					>
+						<Icon name='right' size={30} color='#fff'/>
+					</TouchableOpacity>					
+				</View>
 				<ScrollView style={styles.scroll}>
 					{
-						meets.map((meet)=>{
-							return (
-								<View style={styles.meetup}>
+
+						meets.map((meet, index)=>{
+							return isSameDay(parseISO(meet.date),  dateActual) && (
+								<View key={index} style={styles.meetup}>
 									<Image
 										style={styles.banner}
 										source={{uri: meet.banner.url}}
 									/>
 									<View style={styles.data}>	
+										<Text style={styles.text}>
+											{ formatedDateText(meet.date)}
+										</Text>
 										<Text style={styles.text}>{meet.title}</Text>
 										<Text style={styles.text}>{meet.description}</Text>
 										<Text style={styles.text}>{meet.localization}</Text>
@@ -51,14 +134,15 @@ export function Dashboard( { meets, getMeets } ) {
 										</TouchableOpacity>
 									</View>
 								</View>
-								)
+							) 
+
 						})
 					}
 				</ScrollView>
 				
 			</View>
 		</Background>
-		)
+		) 
 }
 
 const styles = {
@@ -66,7 +150,48 @@ const styles = {
 		height: '100%',
 	},
 	menu: {
-		height: 70
+		flexDirection: 'row',
+		height: 70,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	modalCenter: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	buttonModal: {
+		width: 100,
+		height: 50,
+		backgroundColor: '#f94d6a',
+		borderRadius: 10,
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginTop: 25,
+	},
+	textButtonModal: {
+		color: '#fff',
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	datePicker: {
+		height: 120,
+		width: 200,
+	},
+	modalView: {
+	   marginHorizontal: 30,
+	    backgroundColor: "white",
+	    borderRadius: 20,
+	    padding: 35,
+	    alignItems: "center",
+	    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  
+	textMenu: {
+		color: '#fff',
+		fontSize: 25,
+		fontWeight: 'bold',
+		marginHorizontal: 25
 	},
 	scroll : {
 		flex: 1,
@@ -100,13 +225,13 @@ const styles = {
     	height: 200,
     	borderTopLeftRadius: 6,
     	borderTopRightRadius: 6,
+    	resizeMode: 'stretch'
   	},
   	text: {
   		flex: 1,
   		color: 'rgba(0,0,0,0.4)',
   		fontSize: 16,
   		marginBottom: 4
-
   	}
 };
 
