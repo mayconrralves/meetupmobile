186ea5410abbdf82c22917aef516675daaf82a62
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2021-01-20 15:19:40 -0300

    Adicionado o Modal do Signup que confirma usuário cadastrado

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -2,21 +2,47 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Background from '../components/Background';
 import FormBegin from '../components/FormBegin';
-import { View } from 'react-native';
+import { View, Modal, TouchableOpacity, Text } from 'react-native';
 import { createUser } from '../store/modules/user/actions';
 
 export  function SignUp(props){
+		const [modalVisible, setModalVisible] = useState(false);
 		const [email, setEmail] = useState('');
 		const [password, setPassword ] = useState('');
 		const [name, setName] = useState('');
 		const [confirmPassword, setConfirmPassword] = useState('');
 		const { register, navigation, failure } = props;
+		const login = () => {
+			navigation.navigate('SignIn');
+			setModalVisible(!modalVisible);
+		}
 		const save = () => {
 			register(name, email, password, confirmPassword);
-			// navigation.navigate('SignIn');
+			console.warn(failure)
+			if(failure) {
+				setModalVisible(!modalVisible)
+			}
 		}
 	return (
 		<Background>
+			<Modal
+				        animationType="slide"
+				        transparent={true}
+				        visible={modalVisible}
+				        style={styles.modal}
+				      >
+				      <View style={styles.modalCenter}>
+				      		 <View style={styles.modalView}>
+					      			<Text style={styles.textModal}>Usuário criado com Sucesso!</Text>
+		    					<TouchableOpacity style={styles.buttonModal}
+		    							onPress={login}
+		    					>
+		    						<Text style={styles.textButtonModal}> Entre </Text>
+		    					</TouchableOpacity>
+					      </View>
+				      </View>
+				     
+			</Modal>
 			<FormBegin
 				 {...props} 
 				 signup
@@ -30,6 +56,44 @@ export  function SignUp(props){
 		</Background>
 		
 		)
+}
+const styles = {
+	modalCenter: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	modalView: {
+	   marginHorizontal: 30,
+	    backgroundColor: "white",
+	    borderRadius: 20,
+	    padding: 35,
+	    alignItems: "center",
+	    backgroundColor: 'rgba(0,0,0,0.7)',
+  	},
+  	textModal: {
+		color: '#fff',
+		fontSize: 22,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		marginBottom: 12
+	},
+	buttonModal: {
+		width: 120,
+		height: 60,
+		backgroundColor: '#f94d6a',
+		borderRadius: 10,
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginTop: 25,
+	},
+	
+	textButtonModal: {
+		color: '#fff',
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	
 }
 const mapStateToProps = state => {
 	const { user } = state;
